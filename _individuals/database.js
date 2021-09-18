824513c564db44c52ced64da2762ef0d6f2bc367
chrissteiner <c.steiner16@gmx.at>
2021-09-18 11:16:25 +0200

    fixed db connection

--- _individuals/database.js
+++ _individuals/database.js
@@ -1,6 +1,6 @@
 //PROD
 // module.exports.database_credits = {     connectionLimit:1000,   host: 'remotemysql.com', user: '8KLveBuY3O', database: '8KLveBuY3O',    password: 'rEodRhOBEC',  port: 3306 };
-module.exports.database_credits = {     connectionLimit:10,   host: 'http://83.229.87.18', user: 'chrizly_microservice', database: 'office_chrizly',    password: 'TABry4y9na*H$!_U',  port: 3306 };
+module.exports.database_credits = {     connectionLimit:10,   host: '83.229.87.18', user: 'chrizly_microservice', database: 'office_chrizly',    password: 'TABry4y9na*H$!_U',  port: 3306 };
 // localhost
 // module.exports.database_credits = {     connectionLimit:10,     host: 'localhost',       user: 'root',       database: 'PMC_statistic', password: 'MakeItBig',      port: 3306 };
 
