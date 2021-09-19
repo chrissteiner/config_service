bfac6a56ee0b17d8c80db0399ce673feeffb3b5d
chrissteiner <c.steiner16@gmx.at>
2021-09-19 19:39:55 +0200

    check double entries

--- routes/config_v2.js
+++ routes/config_v2.js
@@ -133,25 +133,43 @@ config.post("/config_service/API/v2/createRFIDCard", (req, res) => {
     const rfid_uid = req.body.rfid_UID.toString(); //const user_email = 'chris_steiner@me.com';
     const aws_id = req.body.aws_id.toString(); //const user_email = 'chris_steiner@me.com';
     if (benutzername != '' && rfid_uid != undefined && aws_id != undefined) {
-        //define MongoDB Query
-        const queryString = SqlString.format("INSERT INTO t_RFID_config (benutzername, rfid_uid, aws_id) VALUES('"+ benutzername + "','"+ rfid_uid +"','"+ aws_id +"');");
+
+        const queryString = SqlString.format("Select * FROM t_RFID_config WHERE rfid_uid = " + rfid_uid + ";");
         logger.debug(req.hostname + req.url + " " + queryString)
 
         getConnection().query(queryString, (err, rows, fields) => {
             if (err) {
                 //throw error if not successful
-                logger.error(req.hostname + req.url + " Insert cannot be done! Error: " + err)
+                logger.error(req.hostname + req.url + " Error in DB communication!: " + err)
                 res.sendStatus(500);
                 return;
             } else {
-                logger.info(req.hostname + req.url + " New Card created: " + rfid_uid);
-                logger.debug("Got Data: %o", rows);
-                logger.debug("Got Rows: %o", rows.length);
-                //res.json(rows)
-                res.status(200).send(rows);
+                if(rows.length > 0){
+                    res.status(400).send("Die Karte mit der ID wurde bereits für " + rows[0].benutzername+ " verwendet");
+                    logger.error("Die Karte mit der ID wurde bereits für " + rows[0].benutzername+ " verwendet");
+                    return;
+                }else{
+                    const queryString = SqlString.format("INSERT INTO t_RFID_config (benutzername, rfid_uid, aws_id) VALUES('"+ benutzername + "','"+ rfid_uid +"','"+ aws_id +"');");
+                    logger.debug(req.hostname + req.url + " " + queryString)
+            
+                    getConnection().query(queryString, (err, rows, fields) => {
+                        if (err) {
+                            //throw error if not successful
+                            logger.error(req.hostname + req.url + " Insert cannot be done! Error: " + err)
+                            res.sendStatus(500);
+                            return;
+                        } else {
+                            logger.info(req.hostname + req.url + " New Card created: " + rfid_uid);
+                            logger.debug("Got Data: %o", rows);
+                            logger.debug("Got Rows: %o", rows.length);
+                            //res.json(rows)
+                            res.status(200).send(rows);
+                        }
+                    })
+                }
             }
         })
-
+        
     }else{
         res.status(500).send("Die Daten liegen in keiner gültigen Form vor");
         logger.error("Die Daten liegen in keiner gültigen Form vor");
